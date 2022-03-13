a2fcd8079d13f74697a393950dd5ef9992da7fcd
Joseph Tharpe <solowun6@aol.com>
2022-03-13 13:41:55 -0700

    uncommented number formatter

--- src/com/codewithjoey/Main.java
+++ src/com/codewithjoey/Main.java
@@ -1,13 +1,14 @@
 package com.codewithjoey;
 
 //import java.text.NumberFormat;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // number formatter
-        //String result = NumberFormat.getPercentInstance().format(0.2);
+        String result = NumberFormat.getPercentInstance().format(0.2);
 
         //Scanner object for use
         Scanner scanner = new Scanner(System.in);
