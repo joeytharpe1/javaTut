dc948d46e50468c152b83efe09eae2c70f9e52bd
Joseph Tharpe <solowun6@aol.com>
2022-03-13 13:27:10 -0700

    java practice

---
+++ src/com/codewithjoey/Main.java
@@ -0,0 +1,20 @@
+package com.codewithjoey;
+
+//import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // number formatter
+        //String result = NumberFormat.getPercentInstance().format(0.2);
+
+        //Scanner object
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name: ");
+        String name = scanner.nextLine();
+        System.out.println("name: " + name);
+
+
+    }
+}
